871d2b7674b6d6cd6ae038c634a03a122bc9cebd
CodeKiro <kirofiniello@outlook.it>
2024-05-24 00:32:05 +0200

    l'esercizio è stato svolto utilizzando la funzione filter sull'array di oggetti

--- snack2/JS/script.js
+++ snack2/JS/script.js
@@ -18,38 +18,58 @@
 
 const elencoStudenti = [
     {
-        Id: 213,
+        Id:213,
         Name: 'Giuseppina della Rovere',
         Grades: 78,
     },
     {
-        Id: 110,
+        Id:110,
         Name: 'Paola Cortellessa',
-        Grades: 96,
+        Grades:96,
     },
     {
-        Id: 250,
+        Id:250,
         Name: 'Andrea Mantegna',
-        Grades: 48,
+        Grades:48,
     },
     {
         Id: 145,
         Name: 'Gaia Borromini  ',
-        Grades: 74,
+        Grades:74,
     },
     {
         Id: 196,
         Name: 'Luigi Grimaldello',
-        Grades: 68,
+        Grades:68,
     },
     {
         Id: 102,
         Name: 'Piero della Francesca',
-        Grades: 50,
+        Grades:50,
     },
     {
         Id: 120,
         Name: 'Francesca da Polenta ',
-        Grades: 84,
+        Grades:84,
     },
 ];
+
+
+let studentiUppercase = elencoStudenti.map(studente => {
+    return {
+        studente,
+        Name: studente.Name.trim().toUpperCase()
+    };
+});
+
+console.log("Studenti con nomi in maiuscolo:", studentiUppercase);
+
+// Filtrare gli studenti con voti superiori a 70
+
+const studenti70plus = elencoStudenti.filter(studente => studente.Grades > 70);
+console.log(studenti70plus);
+
+
+// Filtrare gli studenti con voti superiori a 70 e con id superiore a 120
+let studentiGradesMaggiori70eIdMaggioriDi120 = studenti70plus.filter(studente => studente.Id > 120 );
+console.log(studentiGradesMaggiori70eIdMaggioriDi120);
